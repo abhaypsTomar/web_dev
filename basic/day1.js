1110cce4a409b56c3319761dcdc9859c3a486daa
abhaypsTomar <abhaypratap20315@gmail.com>
2023-07-19 09:53:27 +0530

    first commit

---
+++ basic/day1.js
@@ -0,0 +1,18 @@
+// let objs={college:'chitkara university',roll_no:2110993753,name:"Abhay Pratap Singh Tomar",num_student:[1,2,3,4]}
+// console.log(objs)
+// function sum(a,b){
+//     return a+b;
+    
+// }
+// console.log(sum(1,2))
+var obj={
+    
+    sum:function(a,b){
+        return a+b;
+        
+    },
+    multi:function(a,b){
+        return a*b;
+    }
+}
+console.log(obj)
